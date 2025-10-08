005f79e284bf085232aeb68b2d5b16f0f63fbd6d
Andrew-Hoskins <AndyHoskins87+git@gmail.com>
2025-10-08 20:31:31 +0100

    Created reusable star movie rating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";
 // import App from "./App.jsx";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} onSetRating={setMovieRating} />
+      <p>This Movie was rated {movieRating}</p>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <Test />
   </StrictMode>
 );
